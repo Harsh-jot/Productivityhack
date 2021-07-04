22c504881ecdb7811e662fdc68a62282a951ed24
Harsh-jot <72195283+Harsh-jot@users.noreply.github.com>
2021-07-04 16:34:44 +0530

    Create productivity.py

---
+++ productivity.py
@@ -0,0 +1,9 @@
+print("Productivity Hack")
+print("Are you still Working?")
+
+if input(">_")=="yes":
+  print("So, why are you on this app?")
+else:
+  print("well why not??")
+  input(">_")
+  print("Dont give excuses")
